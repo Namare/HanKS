889cb38c5d7ffe324a3d0d4dd6bf341192c6edad
viktor <Nwebootn@gmail.com>
2024-09-13 02:39:22 +0300

    escape_markdown

--- main.py
+++ main.py
@@ -24,12 +24,17 @@ user_sessions = {}
 
 
 def escape_markdown_v2(text):
-    # Список символов, которые нужно экранировать в MarkdownV2
-    special_characters = r'_*\[\]()~`>#+\-=|{}.!'
+    # Экранируем специальные символы в тексте
+    text = re.sub(r'([_*\[\]()~`>#+\-=|{}.!])', r'\\\1', text)
 
-    # Экранируем каждый специальный символ с помощью обратной косой черты
-    return re.sub(f'([{re.escape(special_characters)}])', r'\\\1', text)
+    # Экранируем символы внутри pre и code блоков
+    text = re.sub(r'(`+)', r'\\\1', text)  # Экранируем обратные кавычки
+    text = re.sub(r'(\\)', r'\\\\', text)  # Экранируем обратные косые черты
 
+    # Экранируем символы внутри ссылок (http://example.com)
+    text = re.sub(r'(\))', r'\\\1', text)
+
+    return text
 def get_gpt4_chat_response(user_id, user_message):
     if user_id not in user_sessions:
         base_char = "Я - Хенкс, енот-программист искуственных интеллект бот асисстент." \
@@ -94,7 +99,6 @@ async def handle_message(message: types.Message):
 
     # Отправляем текстовое сообщение с ответом
     await bot.send_message(chat_id=message.chat.id, text=res, parse_mode="MarkdownV2")
-    print(res)
     audio_file = text_to_speech(res)
 
     # Отправляем голосовое сообщение
